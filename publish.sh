4810b9c870d10ca57b1589e9b91f1515857ad74d
Jan Mewes <jan.mewes@epages.com>
2021-03-11 14:11:58 +0100

    WIP

--- publish.sh
+++ publish.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 set -e # Exit with nonzero exit code if anything fails
 
-if [[ "$TRAVIS_TAG" =~ ^[0-9.]+ ]] ; then
-	./gradlew publishToSonatype --exclude-task :restdocs-server:publishToSonatype --info
-fi
+# if [[ "$TRAVIS_TAG" =~ ^[0-9.]+ ]] ; then
+	./gradlew publishToSonatype \
+		--exclude-task :restdocs-server:publishToSonatype \
+		--info \
+	 	-Dorg.gradle.project.signing.keyId="${SIGNING_KEY_ID}" \
+		-Dorg.gradle.project.signing.password="${SIGNING_PASSWORD}"
+# fi
