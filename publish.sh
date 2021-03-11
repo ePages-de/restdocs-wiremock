bf3a54c2f7d5af60e99bcb49c37428eda65c8ac4
Jan Mewes <jan.mewes@epages.com>
2021-03-11 15:12:08 +0100

    WIP

--- publish.sh
+++ publish.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 set -e # Exit with nonzero exit code if anything fails
 
+# if [[ "$TRAVIS_PULL_REQUEST" = "true" ]]; then
+# 	exit 0
+# fi
+
 # if [[ "$TRAVIS_TAG" =~ ^[0-9.]+ ]] ; then
 	./gradlew publishToSonatype \
 		--exclude-task :restdocs-server:publishToSonatype \
