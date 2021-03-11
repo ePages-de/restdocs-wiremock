0401782f165d96c040da147011079fa015aab4cc
Jan Mewes <jan.mewes@epages.com>
2021-03-11 15:46:34 +0100

    WIP

--- ci_build.sh
+++ ci_build.sh
@@ -3,4 +3,5 @@ set -e # Exit with nonzero exit code if anything fails
 
 ./gradlew clean build jacocoTestReport coveralls \
   -Dorg.gradle.project.signing.keyId="${SIGNING_KEY_ID}" \
-  -Dorg.gradle.project.signing.password="${SIGNING_PASSWORD}"
+  -Dorg.gradle.project.signing.password="${SIGNING_PASSWORD}" \
+  -Dorg.gradle.project.signing.secretKeyRingFile="${SIGNING_KEYRING_FILE}"
