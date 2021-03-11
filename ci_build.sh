2f9a8f7a4a209af7e27538f81f1cdf3fbafa5c5f
Jan Mewes <jan.mewes@epages.com>
2021-03-11 16:01:09 +0100

    WIP

--- ci_build.sh
+++ ci_build.sh
@@ -4,7 +4,7 @@ set -e # Exit with nonzero exit code if anything fails
 ./gradlew clean build jacocoTestReport coveralls \
   -Dorg.gradle.project.signing.keyId="${SIGNING_KEY_ID}" \
   -Dorg.gradle.project.signing.password="${SIGNING_PASSWORD}" \
-  -Dorg.gradle.project.signing.secretKeyRingFile="${TRAVIS_BUILD_DIR}/${SIGNING_KEYRING_FILE}"
+  -Dorg.gradle.project.signing.secretKeyRingFile="${SIGNING_KEYRING_FILE}"
 
 # ./gradlew clean build jacocoTestReport coveralls \
 #   -x signMavenJavaPublication -x signArchives
--- ci_publish.sh
+++ ci_publish.sh
@@ -9,7 +9,9 @@ set -e # Exit with nonzero exit code if anything fails
 	./gradlew publishToSonatype \
 		--exclude-task :restdocs-server:publishToSonatype \
 		--info \
+		-Dorg.gradle.project.sonatypeUsername=${SONATYPE_USERNAME} \
+		-Dorg.gradle.project.sonatypePassword=${SONATYPE_PASSWORD} \
 	 	-Dorg.gradle.project.signing.keyId="${SIGNING_KEY_ID}" \
 		-Dorg.gradle.project.signing.password="${SIGNING_PASSWORD}" \
-		-Dorg.gradle.project.signing.secretKeyRingFile="${TRAVIS_BUILD_DIR}/${SIGNING_KEYRING_FILE}"
+		-Dorg.gradle.project.signing.secretKeyRingFile="${SIGNING_KEYRING_FILE}"
 # fi
