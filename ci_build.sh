763e31cca4262405c67e35f00ab47666ff1ce7ce
Jan Mewes <jan.mewes@epages.com>
2023-05-11 18:05:01 +0200

    Add gradle properties

--- ci_build.sh
+++ ci_build.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
 set -e # Exit with nonzero exit code if anything fails
 
-./gradlew clean build \
+./gradlew  \
+  -Dorg.gradle.project.sonar.projectKey="ePages-de_restdocs-wiremock" \
+  -Dorg.gradle.project.sonar.organization="epages-de" \
+  -Dorg.gradle.project.sonar.host.url="https://sonarcloud.io" \
+  clean  \
+  build \
+  sonarqube \
+  --info \
   --exclude-task signMavenJavaPublication \
   --exclude-task signArchives \
-  sonarqube --info
