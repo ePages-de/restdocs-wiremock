bd6075dcc1655266155dc1edec0ba0fe8821b2ef
Jan Mewes <jan.mewes@epages.com>
2023-05-11 17:26:47 +0200

    Fix typo in shell script

--- ci_build.sh
+++ ci_build.sh
@@ -3,5 +3,5 @@ set -e # Exit with nonzero exit code if anything fails
 
 ./gradlew clean build \
   --exclude-task signMavenJavaPublication \
-  --exclude-task signArchives
+  --exclude-task signArchives \
   sonarqube --info
