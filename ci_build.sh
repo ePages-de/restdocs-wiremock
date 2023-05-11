be7cb9df699f8a60897cdf7c9696a02ee0907714
Jan Mewes <jan.mewes@epages.com>
2023-05-11 17:17:52 +0200

    EPNG-10282: Run pull request tests

--- ci_build.sh
+++ ci_build.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 set -e # Exit with nonzero exit code if anything fails
 
-./gradlew clean build jacocoTestReport coveralls \
+./gradlew clean build \
   --exclude-task signMavenJavaPublication \
   --exclude-task signArchives
+  sonarqube --info
