229e65fe0613dda336d54698f8e07fccd8046e3a
Jan Mewes <jan.mewes@epages.com>
2023-05-12 16:56:22 +0200

    Add publish workflow

--- ci_publish.sh
+++ ci_publish.sh
@@ -1,16 +1,32 @@
 #!/bin/bash
+
 set -e # Exit with nonzero exit code if anything fails
 
-openssl aes-256-cbc -K $encrypted_c2f58914b0fd_key -iv $encrypted_c2f58914b0fd_iv \
-	-in secret-keys.gpg.enc -out "${SIGNING_KEYRING_FILE}" -d
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
-if [[ "$TRAVIS_TAG" =~ ^[0-9.]+ ]] ; then
-	./gradlew publishToSonatype \
-		--exclude-task :restdocs-server:publishToSonatype \
-		--info \
-		-Dorg.gradle.project.sonatypeUsername="${SONATYPE_USERNAME}" \
-		-Dorg.gradle.project.sonatypePassword="${SONATYPE_PASSWORD}" \
-	 	-Dorg.gradle.project.signing.keyId="${SIGNING_KEY_ID}" \
-		-Dorg.gradle.project.signing.password="${SIGNING_PASSWORD}" \
-		-Dorg.gradle.project.signing.secretKeyRingFile="${SIGNING_KEYRING_FILE}"
-fi
+function check_variable_set() {
+  _VARIABLE_NAME=$1
+	_VARIABLE_VALUE=${!_VARIABLE_NAME}
+	if [[ -z ${_VARIABLE_VALUE} ]] ; then
+		echo "Missing env variable ${_VARIABLE_NAME}"
+		exit 1
+	fi
+}
+check_variable_set FILE_ENCRYPTION_PASSWORD
+check_variable_set SIGNING_KEY_ID
+check_variable_set SIGNING_PASSWORD
+check_variable_set SONATYPE_USERNAME
+check_variable_set SONATYPE_PASSWORD
+
+# # Decrypt signing key
+gpg --quiet --batch --yes --decrypt --passphrase="${FILE_ENCRYPTION_PASSWORD}" \
+	--output secret-keys.gpg secret-keys.gpg.enc
+
+# Publish
+./gradlew publishToSonatype \
+	--info \
+	-Dorg.gradle.project.sonatypeUsername="${SONATYPE_USERNAME}" \
+	-Dorg.gradle.project.sonatypePassword="${SONATYPE_PASSWORD}" \
+	-Dorg.gradle.project.signing.keyId="${SIGNING_KEY_ID}" \
+	-Dorg.gradle.project.signing.password="${SIGNING_PASSWORD}" \
+	-Dorg.gradle.project.signing.secretKeyRingFile="${SCRIPT_DIR}/secret-keys.gpg"
