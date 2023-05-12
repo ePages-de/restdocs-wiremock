23f4228599e4b581c06f9366a2e0509ceb5fa97d
Jan Mewes <jan.mewes@epages.com>
2023-05-12 09:49:52 +0200

    Drop sonar support, for now

--- ci_build.sh
+++ ci_build.sh
@@ -2,10 +2,6 @@
 set -e # Exit with nonzero exit code if anything fails
 
 ./gradlew  \
-  -Dsonar.projectKey="ePages-de_restdocs-wiremock" \
-  -Dsonar.organization="epages-de" \
-  -Dsonar.host.url="https://sonarcloud.io" \
-  -Dsonar.login="$SONAR_TOKEN" \
   clean  \
   build \
   sonarqube \
