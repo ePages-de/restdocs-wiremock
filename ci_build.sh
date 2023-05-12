c32056edda630a99b9ace8e412d335c22eb6db8c
Jan Mewes <jan.mewes@epages.com>
2023-05-12 09:55:53 +0200

    Skip sonar, for now

--- ci_build.sh
+++ ci_build.sh
@@ -2,9 +2,7 @@
 set -e # Exit with nonzero exit code if anything fails
 
 ./gradlew  \
-  clean  \
   build \
-  sonarqube \
   --info \
   --exclude-task signMavenJavaPublication \
   --exclude-task signArchives \
