b6b73d98d13f5ee6c3f06b8c31b02da6d2aeeece
Oliver Zscheyge <o.zscheyge@epages.com>
2023-06-01 13:00:06 +0200

    Bump to Spring Boot 2.7

--- wiremock-spring-boot-starter/src/main/java/com/epages/wiremock/starter/WireMockAutoConfiguration.java
+++ wiremock-spring-boot-starter/src/main/java/com/epages/wiremock/starter/WireMockAutoConfiguration.java
@@ -2,19 +2,19 @@ package com.epages.wiremock.starter;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
+import org.springframework.boot.autoconfigure.AutoConfiguration;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
 
 import com.github.tomakehurst.wiremock.WireMockServer;
 import com.github.tomakehurst.wiremock.common.ClasspathFileSource;
 import com.github.tomakehurst.wiremock.core.WireMockConfiguration;
 import com.github.tomakehurst.wiremock.extension.responsetemplating.ResponseTemplateTransformer;
 
-@Configuration
+@AutoConfiguration
 @ConditionalOnClass({WireMockServer.class})
 @ConditionalOnProperty(name="wiremock.enabled", havingValue="true")
 @EnableConfigurationProperties(WireMockProperties.class)
